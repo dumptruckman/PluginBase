a58b9debf9f941077401c8322a8ea7e1e33c65e3
Jeremy Wood <farachan@gmail.com>
2012-04-09 14:11:06 -0400

    Made BukkitPlugin extend org.bukkit.plugin.Plugin

--- bukkit/src/main/java/com/dumptruckman/minecraft/pluginbase/plugin/BukkitPlugin.java
+++ bukkit/src/main/java/com/dumptruckman/minecraft/pluginbase/plugin/BukkitPlugin.java
@@ -3,8 +3,9 @@ package com.dumptruckman.minecraft.pluginbase.plugin;
 import com.dumptruckman.minecraft.pluginbase.config.BaseConfig;
 import com.dumptruckman.minecraft.pluginbase.locale.Messager;
 import com.pneumaticraft.commandhandler.CommandHandler;
+import org.bukkit.plugin.Plugin;
 
-public interface BukkitPlugin<C extends BaseConfig> extends PluginBase<C> {
+public interface BukkitPlugin<C extends BaseConfig> extends PluginBase<C>, Plugin {
     
     CommandHandler getCommandHandler();
 
