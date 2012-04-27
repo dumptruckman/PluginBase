bbe4b8560a0cb7da67db4e5457a20131c4b832ea
Jeremy Wood <farachan@gmail.com>
2012-04-27 10:34:37 -0400

    Removed odd OP only requirement.

--- bukkit/src/main/java/com/dumptruckman/minecraft/pluginbase/permission/PermHandler.java
+++ bukkit/src/main/java/com/dumptruckman/minecraft/pluginbase/permission/PermHandler.java
@@ -12,21 +12,11 @@ public class PermHandler implements PermissionsInterface {
 
     @Override
     public boolean hasPermission(CommandSender commandSender, String node, boolean opRequired) {
-        if (opRequired) {
-            if (!commandSender.isOp()) {
-                return false;
-            }
-        }
         return commandSender.hasPermission(node);
     }
 
     @Override
     public boolean hasAnyPermission(CommandSender commandSender, List<String> nodes, boolean opRequired) {
-        if (opRequired) {
-            if (!commandSender.isOp()) {
-                return false;
-            }
-        }
         for (String node : nodes) {
             if (commandSender.hasPermission(node)) {
                 return true;
@@ -37,11 +27,6 @@ public class PermHandler implements PermissionsInterface {
 
     @Override
     public boolean hasAllPermission(CommandSender commandSender, List<String> nodes, boolean opRequired) {
-        if (opRequired) {
-            if (!commandSender.isOp()) {
-                return false;
-            }
-        }
         for (String node : nodes) {
             if (!commandSender.hasPermission(node)) {
                 return false;
