54cc4d61e821aef5c3482fa1aeb7c8e24bb21dcd
Jeremy Wood <farachan@gmail.com>
2012-06-27 09:07:23 -0400

    Cleaned up SQL lib some. again.

--- base/src/main/java/com/dumptruckman/minecraft/pluginbase/database/SQLDatabase.java
+++ base/src/main/java/com/dumptruckman/minecraft/pluginbase/database/SQLDatabase.java
@@ -2,7 +2,6 @@ package com.dumptruckman.minecraft.pluginbase.database;
 
 import com.dumptruckman.minecraft.pluginbase.plugin.PluginBase;
 
-import java.net.MalformedURLException;
 import java.sql.ResultSet;
 
 public interface SQLDatabase {
@@ -17,7 +16,7 @@ public interface SQLDatabase {
 
     boolean createTable(String query);
 
-    boolean checkTable(String name) throws MalformedURLException, InstantiationException, IllegalAccessException;
+    boolean checkTable(String name);
 
-    boolean wipeTable(String table) throws MalformedURLException, InstantiationException, IllegalAccessException;
+    boolean wipeTable(String table);
 }
