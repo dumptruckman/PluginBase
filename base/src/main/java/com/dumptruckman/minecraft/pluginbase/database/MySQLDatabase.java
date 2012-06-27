e390c8efb88756f47476213898ee54b4549c7d84
Jeremy Wood <farachan@gmail.com>
2012-06-27 16:23:58 -0400

    Fixed ordering of mysql info.

--- base/src/main/java/com/dumptruckman/minecraft/pluginbase/database/MySQLDatabase.java
+++ base/src/main/java/com/dumptruckman/minecraft/pluginbase/database/MySQLDatabase.java
@@ -2,9 +2,6 @@ package com.dumptruckman.minecraft.pluginbase.database;
 
 import com.dumptruckman.minecraft.pluginbase.config.BaseConfig;
 import com.dumptruckman.minecraft.pluginbase.plugin.PluginBase;
-import com.dumptruckman.minecraft.pluginbase.util.Logging;
-
-import java.sql.ResultSet;
 
 public class MySQLDatabase extends SQLDB {
 
@@ -17,8 +14,8 @@ public class MySQLDatabase extends SQLDB {
     protected DatabaseHandler newDB(PluginBase plugin) {
         return new MySQL(plugin.config().get(BaseConfig.DB_HOST),
                 plugin.config().get(BaseConfig.DB_PORT),
+                plugin.config().get(BaseConfig.DB_DATABASE),
                 plugin.config().get(BaseConfig.DB_USER),
-                plugin.config().get(BaseConfig.DB_PASS),
-                plugin.config().get(BaseConfig.DB_DATABASE));
+                plugin.config().get(BaseConfig.DB_PASS));
     }
 }
