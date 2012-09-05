9f3786bc655c6230c25dff688d3b3bd360a42f28
Jeremy Wood <farachan@gmail.com>
2012-09-05 19:53:30 -0400

    Duration.valueOf(String) now detects all string time formats allowable.

--- base/src/main/java/com/dumptruckman/minecraft/pluginbase/util/time/Duration.java
+++ base/src/main/java/com/dumptruckman/minecraft/pluginbase/util/time/Duration.java
@@ -1,25 +1,38 @@
 package com.dumptruckman.minecraft.pluginbase.util.time;
 
+import java.text.ParseException;
+
 public final class Duration {
 
-    public static Duration valueOf(String shortForm) {
-        return new Duration(shortForm);
+    public static Duration valueOf(final String shortForm) {
+        long time = -1;
+        try {
+            time = TimeTools.fromShortForm(shortForm);
+        } catch (ParseException ignore) { }
+        if (time == -1) {
+            try {
+                time = Long.valueOf(shortForm);
+            } catch (NumberFormatException ignore) { }
+        }
+        if (time == -1) {
+            try {
+                time = TimeTools.fromLongForm(shortForm);
+            } catch (ParseException ignore) { }
+        }
+        if (time < 0) {
+            time = 0;
+        }
+        return new Duration(time);
     }
 
-    public static Duration valueOf(long duration) {
+    public static Duration valueOf(final long duration) {
         return new Duration(duration);
     }
 
     private final long duration;
     private final String shortForm, longForm;
 
-    private Duration(String shortForm) {
-        this.shortForm = shortForm;
-        this.duration = TimeTools.fromShortForm(shortForm);
-        this.longForm = TimeTools.toLongForm(duration);
-    }
-
-    private Duration(long duration) {
+    private Duration(final long duration) {
         this.duration = duration;
         this.longForm = TimeTools.toLongForm(duration);
         this.shortForm = TimeTools.toShortForm(duration);
