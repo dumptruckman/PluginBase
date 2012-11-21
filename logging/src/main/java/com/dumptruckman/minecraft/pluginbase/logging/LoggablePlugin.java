71b58a970c0a42acd93de3375396d79f1527c73c
Jeremy Wood <farachan@gmail.com>
2012-11-21 15:39:30 -0500

    LoggablePlugin now 100% checkstyled.

--- logging/src/main/java/com/dumptruckman/minecraft/pluginbase/logging/LoggablePlugin.java
+++ logging/src/main/java/com/dumptruckman/minecraft/pluginbase/logging/LoggablePlugin.java
@@ -5,9 +5,23 @@ package com.dumptruckman.minecraft.pluginbase.logging;
 
 import java.io.File;
 
+/**
+ * Any plugin wishing to use the PluginLogger must implement this method.  In a Bukkit plugin this is as simple
+ * as adding this class in an implements clause.
+ */
 public interface LoggablePlugin {
 
+    /**
+     * Returns the name of the plugin.
+     *
+     * @return The name of the plugin.
+     */
     String getName();
 
+    /**
+     * Returns the folder where a plugin's data is stored.
+     *
+     * @return Returns the folder where a plugin's data is stored.
+     */
     File getDataFolder();
 }
