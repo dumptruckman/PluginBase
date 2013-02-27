cfd14d7b06c057f6b0114d582639ffa5b80efe64
Jeremy Wood <farachan@gmail.com>
2013-02-27 13:57:55 -0500

    Added additional notes in package-info for properties.

--- properties/src/main/java/com/dumptruckman/minecraft/pluginbase/properties/package-info.java
+++ properties/src/main/java/com/dumptruckman/minecraft/pluginbase/properties/package-info.java
@@ -1,6 +1,8 @@
 /**
  * Classes used for managing object properties.
  * <p/>
+ * See {@link PropertyFactory} for details on creating your own Property objects.
+ * <p/>
  * This is part of com.dumptruckman.minecraft.pluginbase:Properties.
  */
 package com.dumptruckman.minecraft.pluginbase.properties;
