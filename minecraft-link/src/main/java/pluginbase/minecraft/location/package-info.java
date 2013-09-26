d9c041506768d07ed93890a4dce973df1bf618e6
Jeremy Wood <farachan@gmail.com>
2013-09-26 11:18:16 -0400

    Renamed some modules for consistency.

--- minecraft-link/src/main/java/pluginbase/minecraft/location/package-info.java
+++ minecraft-link/src/main/java/pluginbase/minecraft/location/package-info.java
@@ -1,7 +1,7 @@
 /**
  * A collection of classes used to represent locations/coordinates in the Minecraft environment.
  * <p/>
- * This is part of com.dumptruckman.minecraft.pluginbase:MinecraftLink.
+ * This is part of com.dumptruckman.minecraft.pluginbase:Minecraft-Link.
  */
 package pluginbase.minecraft.location;
 
--- minecraft-link/src/main/java/pluginbase/minecraft/package-info.java
+++ minecraft-link/src/main/java/pluginbase/minecraft/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Contains features that help PluginBase recognize facets of Minecraft such as entities and players among other things.
  * <p/>
- * This is part of com.dumptruckman.minecraft.pluginbase:MinecraftLink.
+ * This is part of com.dumptruckman.minecraft.pluginbase:Minecraft-Link.
  */
 package pluginbase.minecraft;
 
