4f4d9b47da522b2afc54a53abfd66b51cdba4d85
Ali Moghnieh <me@SupaHam.com>
2014-06-18 03:06:13 +0100

    Updated javadocs for command module.

--- commands/src/main/java/pluginbase/command/package-info.java
+++ commands/src/main/java/pluginbase/command/package-info.java
@@ -2,7 +2,7 @@
  * Contains all the classes necessary for adding platform independent commands to a Minecraft plugin.
  * <p/>
  * CommandHandler must be implemented for the specific server implementation and your plugin class needs to implement
- * {@link CommandProvider} and {@link pluginbase.messages.messaging.Messaging}.
+ * {@link pluginbase.command.CommandProvider} and {@link pluginbase.messages.messaging.Messaging}.
  * <p/>
  * Part of com.supaham.pluginbase:Commands.
  */
