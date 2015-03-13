b4e5279a69a490511f4e3cef6389dccbb8b05d4c
Jeremy Wood <farachan@gmail.com>
2015-03-13 14:53:57 -0400

    HoconSerializableConfigLoader should now serialize. Comments are not setup correctly.

--- sponge/serializable-config-sponge/src/main/java/pluginbase/sponge/config/HoconSerializableConfigLoader.java
+++ sponge/serializable-config-sponge/src/main/java/pluginbase/sponge/config/HoconSerializableConfigLoader.java
@@ -1,25 +1,45 @@
 package pluginbase.sponge.config;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
 import com.google.common.io.CharSink;
 import com.google.common.io.CharSource;
+import com.typesafe.config.ConfigList;
+import com.typesafe.config.ConfigObject;
+import com.typesafe.config.ConfigOrigin;
 import com.typesafe.config.ConfigParseOptions;
 import com.typesafe.config.ConfigRenderOptions;
+import com.typesafe.config.ConfigValue;
+import com.typesafe.config.ConfigValueFactory;
 import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.ConfigurationOptions;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.commented.SimpleCommentedConfigurationNode;
 import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
+import pluginbase.config.ConfigSerializer;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class HoconSerializableConfigLoader extends HoconConfigurationLoader {
 
+    public static final Pattern CRLF_MATCH = Pattern.compile("\r\n?");
+    private final ConfigRenderOptions render;
+    private final ConfigParseOptions parse;
+
     public HoconSerializableConfigLoader(CharSource source, CharSink sink, ConfigRenderOptions render, ConfigParseOptions parse, boolean preservesHeader) {
         super(source, sink, render, parse, preservesHeader);
+        this.render = render;
+        this.parse = parse;
     }
 
     public static class Builder extends HoconConfigurationLoader.Builder {
@@ -88,11 +108,23 @@ public class HoconSerializableConfigLoader extends HoconConfigurationLoader {
     @Override
     public void loadInternal(CommentedConfigurationNode node, BufferedReader reader) throws IOException {
         super.loadInternal(node, reader);
+        node.setValue(ConfigSerializer.deserialize(node.getValue()));
     }
 
     @Override
     protected void saveInternal(ConfigurationNode node, Writer writer) throws IOException {
-        super.saveInternal(node, writer);
+        if (!node.hasMapChildren()) {
+            if (node.getValue() == null) {
+                writer.write(LINE_SEPARATOR);
+                return;
+            } else {
+                throw new IOException("HOCON cannot write nodes not in map format!");
+            }
+        }
+        final ConfigValue value = ConfigValueFactory.fromAnyRef(ConfigSerializer.serialize(node.getValue()), "serializable-config-hocon");
+        traverseForComments(value, node);
+        final String renderedValue = value.render(render);
+        writer.write(renderedValue);
     }
 
     @Override
@@ -100,4 +132,62 @@ public class HoconSerializableConfigLoader extends HoconConfigurationLoader {
         return SimpleCommentedConfigurationNode.root(options);
     }
 
+    private void traverseForComments(ConfigValue value, ConfigurationNode node) throws IOException {
+        potentialComment(value, node);
+        switch (value.valueType()) {
+            case OBJECT:
+                for (Map.Entry<Object, ? extends ConfigurationNode> ent : node.getChildrenMap().entrySet()) {
+                    traverseForComments(((ConfigObject) value).get(ent.getKey().toString()), ent.getValue());
+                }
+                break;
+            case LIST:
+                List<? extends ConfigurationNode> nodes = node.getChildrenList();
+                for (int i = 0; i < nodes.size(); ++i) {
+                    traverseForComments(((ConfigList) value).get(i), nodes.get(i));
+                }
+                break;
+        }
+    }
+
+    // -- Comment handling -- this might have to be updated as the hocon dep changes (But tests should detect this
+    // breakage
+    private static final Class<? extends ConfigValue> VALUE_CLASS;
+    private static final Class<? extends ConfigOrigin> ORIGIN_CLASS;
+    private static final Field VALUE_ORIGIN;
+    private static final Method ORIGIN_SET_COMMENTS;
+    static {
+        try {
+            VALUE_CLASS = Class.forName("com.typesafe.config.impl.AbstractConfigValue").asSubclass(ConfigValue.class);
+        } catch (ClassNotFoundException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+
+        ORIGIN_CLASS = ConfigValueFactory.fromAnyRef("a").origin().getClass();
+        try {
+            VALUE_ORIGIN = VALUE_CLASS.getDeclaredField("origin");
+            ORIGIN_SET_COMMENTS = ORIGIN_CLASS.getDeclaredMethod("setComments", List.class);
+            VALUE_ORIGIN.setAccessible(true);
+            ORIGIN_SET_COMMENTS.setAccessible(true);
+        } catch (NoSuchFieldException | NoSuchMethodException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+
+    }
+    private ConfigValue potentialComment(ConfigValue value, ConfigurationNode node) throws IOException {
+        if (!(node instanceof CommentedConfigurationNode)) {
+            return value;
+        }
+        CommentedConfigurationNode commentedNode = (CommentedConfigurationNode) node;
+        Optional<String> comment = commentedNode.getComment();
+        if (!comment.isPresent()) {
+            return value;
+        }
+        try {
+            Object o = ORIGIN_SET_COMMENTS.invoke(value.origin(), ImmutableList.copyOf(LINE_SPLITTER.split(comment.get())));
+            VALUE_ORIGIN.set(value, o);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new IOException("Unable to set comments for config value" + value);
+        }
+        return value;
+    }
 }
