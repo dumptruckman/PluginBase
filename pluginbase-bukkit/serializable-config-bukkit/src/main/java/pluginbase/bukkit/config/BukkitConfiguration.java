371d9920a304dc66efe4626aea3e1ca6a95fb574
Jeremy Wood <farachan@gmail.com>
2015-07-08 12:22:46 -0400

    Force UTF8 file encoding for BukkitConfiguration.

--- pluginbase-bukkit/serializable-config-bukkit/src/main/java/pluginbase/bukkit/config/BukkitConfiguration.java
+++ pluginbase-bukkit/serializable-config-bukkit/src/main/java/pluginbase/bukkit/config/BukkitConfiguration.java
@@ -1,5 +1,7 @@
 package pluginbase.bukkit.config;
 
+import com.google.common.base.Charsets;
+import org.apache.commons.lang.Validate;
 import pluginbase.config.ConfigSerializer;
 import pluginbase.config.SerializationRegistrar;
 import com.google.common.io.Files;
@@ -16,12 +18,14 @@ import pluginbase.messages.messaging.SendablePluginBaseException;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.nio.charset.Charset;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -83,6 +87,29 @@ public abstract class BukkitConfiguration extends FileConfiguration {
         }
 
         return config;
+    }
+
+    @Override
+    public void load(@NotNull File file) throws FileNotFoundException, IOException, InvalidConfigurationException {
+        final FileInputStream stream = new FileInputStream(file);
+
+        load(stream);
+    }
+
+    @Override
+    public void load(@NotNull final InputStream stream) throws IOException, InvalidConfigurationException {
+        load(new InputStreamReader(stream, Charsets.UTF_8));
+    }
+
+    @Override
+    public void save(@NotNull final File file) throws IOException {
+        Files.createParentDirs(file);
+
+        String data = saveToString();
+
+        try (Writer writer = new OutputStreamWriter(new FileOutputStream(file), Charsets.UTF_8)) {
+            writer.write(data);
+        }
     }
 
     @Override
